5678a348f2e0bc1c6ff371c752c56632c9695d7b
gazzaar <gazzarDev@gmail.com>
2025-08-21 15:08:47 +0300

    initial navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+const user = {};
+const NavBar = () => {
+  return (
+    <header className='navbar'>
+      <nav>
+        <Link href={'/'}>
+          <Image
+            src='/assets/icons/logo.svg'
+            alt='logo'
+            width={32}
+            height={32}
+          />
+          <h1>SnapCast</h1>
+        </Link>
+        {user && (
+          <figure>
+            <button>
+              <Image
+                src='/assets/images/dummy.jpg'
+                alt='user'
+                width={36}
+                height={36}
+                className='rounded-full aspect-square'
+              />
+            </button>
+
+            <button>
+              <Image
+                src='assets/icons/logout.svg'
+                alt='logout'
+                width={24}
+                height={24}
+                className='rotate-180'
+              />
+            </button>
+          </figure>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
