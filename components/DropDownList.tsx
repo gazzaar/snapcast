c687b2bfbe9defa76e129114f94ab559372e9050
gazzaar <gazzarDev@gmail.com>
2025-08-27 14:10:09 +0300

    fix typo

--- components/DropDownList.tsx
+++ components/DropDownList.tsx
@@ -1,33 +1,33 @@
 'use client';
 import Image from 'next/image';
 import { useState } from 'react';
-const DropDownList = () => {
+const DropdownList = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="relative">
-      <div className="cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
-        <div className="filter-trigger">
+    <div className='relative'>
+      <div className='cursor-pointer' onClick={() => setIsOpen(!isOpen)}>
+        <div className='filter-trigger'>
           <figure>
             <Image
-              src="/assets/icons/hamburger.svg"
-              alt="menu"
+              src='/assets/icons/hamburger.svg'
+              alt='menu'
               width={14}
               height={14}
             />
             Most recent
           </figure>
           <Image
-            src="/assets/icons/arrow-down.svg"
-            alt="arrow-down"
+            src='/assets/icons/arrow-down.svg'
+            alt='arrow-down'
             width={20}
             height={20}
           />
         </div>
       </div>
       {isOpen && (
-        <ul className="dropdown">
+        <ul className='dropdown'>
           {['Most recent', 'Most liked'].map((option) => (
-            <li key={option} className="list-item">
+            <li key={option} className='list-item'>
               {option}
             </li>
           ))}
@@ -37,4 +37,4 @@ const DropDownList = () => {
   );
 };
 
-export default DropDownList;
+export default DropdownList;
